1b9a9ff48b1808960dea34c6bcc8e2deac23b4d5
tam-mobile <tam.dm@zetagroup.vn>
2022-05-30 17:58:17 +0700

    init 2

--- lib/components/text-field-input.dart
+++ lib/components/text-field-input.dart
@@ -1,21 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/components/style.dart';
 import 'package:my_app/utils/app_utils.dart';
 
 class TextInputCustom extends StatefulWidget {
-  const TextInputCustom({Key? key, String label = "", String hintLabel = "", double height = 40}) : super(key: key);
-
+  const TextInputCustom(
+      {Key? key,
+      this.label = "",
+      this.hintLabel = "",
+      this.height = 40,
+      this.obscureText = false,
+      this.icon,
+      this.prefixIcon,
+      this.suffixIcon,
+      this.suffixPress,
+      required this.controller})
+      : super(key: key);
+  final String label;
+  final String hintLabel;
+  final double height;
+  final bool obscureText;
+  final Icon? icon;
+  final Icon? prefixIcon;
+  final IconData? suffixIcon;
+  final Function? suffixPress;
+  final TextEditingController controller;
   @override
   State<TextInputCustom> createState() => _TextInputCustomState();
 }
 
 class _TextInputCustomState extends State<TextInputCustom> {
-  var _border;
+  late TextEditingController _controller = widget.controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      final String text = _controller.text.toLowerCase();
+      _controller.value = _controller.value.copyWith(
+        text: text,
+        selection: TextSelection(baseOffset: text.length, extentOffset: text.length),
+        composing: TextRange.empty,
+      );
+    });
+  }
+
 //   double _height = Responsive.scale(this.heigh,);
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      decoration: Style.textFieldStyle(),
+      controller: widget.controller,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(16),
+        labelText: widget.label,
+        hintText: widget.hintLabel,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        icon: widget.icon,
+        prefixIcon: widget.prefixIcon,
+        suffixIcon: IconButton(
+          onPressed: () {},
+          icon: Icon(widget.suffixIcon),
+          iconSize: 20,
+          highlightColor: Colors.amber,
+        ),
+      ),
+      obscureText: widget.obscureText,
     );
   }
 }
--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/components/style.dart';
 import 'package:my_app/utils/app_utils.dart';
+import '../../components/button-custom.dart';
 import '../../components/tabbar-custom.dart';
+import '../../components/text-field-input.dart';
 import '../../constains/images.dart';
+import '../../constains/style.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -13,11 +15,35 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStateMixin {
   late TabController _tabController;
-
+  final TextEditingController _sdtController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
   @override
   void initState() {
     _tabController = new TabController(length: 2, vsync: this);
     super.initState();
+    _sdtController.addListener(() {
+      final String text = _sdtController.text.toLowerCase();
+      _sdtController.value = _sdtController.value.copyWith(
+        text: text,
+        selection: TextSelection(baseOffset: text.length, extentOffset: text.length),
+        composing: TextRange.empty,
+      );
+    });
+    _passController.addListener(() {
+      final String text = _passController.text.toLowerCase();
+      _passController.value = _passController.value.copyWith(
+        text: text,
+        selection: TextSelection(baseOffset: text.length, extentOffset: text.length),
+        composing: TextRange.empty,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _sdtController.dispose();
+    _passController.dispose();
+    super.dispose();
   }
 
   @override
@@ -25,36 +51,56 @@ class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStat
     return Scaffold(
       body: Column(
         children: [
-          //   Container(
-          //     decoration: BoxDecoration(border: Border.all(width: 1)),
-          //     height: Responsive.scale(20, context),
-          //   ),
           Expanded(
             flex: 5,
             child: Image.asset(Images.logo),
           ),
           Expanded(
-              flex: 5,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Phone Number',
-                      ),
-                    ),
+            flex: 5,
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                  child: TextInputCustom(
+                    label: "Phone Number",
+                    // hintLabel: "Phone Number",
+                    obscureText: true,
+                    suffixIcon: Icons.phone,
+                    controller: _sdtController,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
-                    child: TextFormField(
-                      decoration: Style.textFieldStyle(labelTextStr: "Password", borderRadiusCircular: 8),
+                ),
+                const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                    child: TextInputCustom(
+                      label: "Password",
                       obscureText: true,
-                    ),
+                      suffixIcon: Icons.remove_red_eye_outlined,
+                      controller: _passController,
+                    )),
+                SizedBox(height: Responsive.scale(30, context)),
+                RoundedButton(
+                  title: "Signin",
+                  onPressed: () {
+                    print("Tapped Me");
+                  },
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text("Chưa có tài khoản."),
+                      TextButton(
+                          onPressed: () {
+                            print("register");
+                          },
+                          child: Text("Đăng ký ngay"))
+                    ],
                   ),
-                ],
-              ))
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
