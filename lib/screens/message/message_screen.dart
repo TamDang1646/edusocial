901e18a4c8f36c0ee7e437c3150e507e8086a68a
tam-mobile <tam.dm@zetagroup.vn>
2022-06-30 09:30:20 +0700

    launcher_icon

--- lib/screens/message/message_screen.dart
+++ lib/screens/message/message_screen.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-import '../../widgets/bottom_bar/bottom_bar.dart';
-
 class MessageScreen extends StatefulWidget {
   const MessageScreen({super.key});
 
